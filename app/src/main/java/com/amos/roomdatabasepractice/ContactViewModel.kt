d8d03e7feebacb6ab0a7683915c97df2939406d0
Humanoid Rebel <60774863+heamoe@users.noreply.github.com>
2024-08-17 16:57:27 -0400

    add comments

--- app/src/main/java/com/amos/roomdatabasepractice/ContactViewModel.kt
+++ app/src/main/java/com/amos/roomdatabasepractice/ContactViewModel.kt
@@ -9,6 +9,15 @@ class ContactViewModel(
     private val _sortType = MutableStateFlow(SortType.FIRST_NAME)
     private val state = MutableStateFlow(ContactState())
     fun onEvent(event:ContactEvent){
-        //try
+        when(event){
+            is ContactEvent.DeleteContact -> TODO()
+            ContactEvent.HideDialog -> TODO()
+            ContactEvent.SaveContact -> TODO()
+            is ContactEvent.SetFirstName -> TODO()
+            is ContactEvent.SetLastName -> TODO()
+            is ContactEvent.SetPhoneNumber -> TODO()
+            ContactEvent.ShowDialog -> TODO()
+            is ContactEvent.SortContacts -> TODO()
+        }
     }
 }
