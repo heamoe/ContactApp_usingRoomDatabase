34c0fc03008ac195c32ed414600bf192e349fccf
Humanoid Rebel <60774863+heamoe@users.noreply.github.com>
2024-08-20 10:36:06 -0400

    update

--- app/src/main/java/com/amos/roomdatabasepractice/ContactViewModel.kt
+++ app/src/main/java/com/amos/roomdatabasepractice/ContactViewModel.kt
@@ -1,7 +1,9 @@
 package com.amos.roomdatabasepractice
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.launch
 
 class ContactViewModel(
     private val  dao: ContactDao
@@ -10,7 +12,9 @@ class ContactViewModel(
     private val state = MutableStateFlow(ContactState())
     fun onEvent(event:ContactEvent){
         when(event){
-            is ContactEvent.DeleteContact -> TODO()
+            is ContactEvent.DeleteContact -> {
+                viewModelScope.launch { dao.deleteContact(event.contact) }
+            }
             ContactEvent.HideDialog -> TODO()
             ContactEvent.SaveContact -> TODO()
             is ContactEvent.SetFirstName -> TODO()
